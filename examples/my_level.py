a5086164275cee7c8e25ecefa6e92fed2ea7e18e
Ailamar Alves <ailamar.alvesg@gmail.com>
2019-07-02 15:43:01 -0300

    Plataforma

--- examples/my_level.py
+++ examples/my_level.py
@@ -12,7 +12,7 @@ class Game(ge.Platformer):
 
     title = 'Candy Shop'
     player_initial_tile = 4, 1
-    final = 50
+    final = 80
     world_theme = 'green'
     player_theme = 'grey'
     #background_theme = 'brown'   
@@ -20,6 +20,7 @@ class Game(ge.Platformer):
     def init_world(self):
         # Inicio, Fim e chão
         self.create_tower(10, 2, coords=(0, 1))
+        self.create_arrow('right', (3, 1))
         self.create_ground(self.final, coords=(0, 0), smooth_ends=True)
         self.create_platform(1, coords=(20, 90))
         self.create_tower(10, coords=(self.final - 1, 1))
@@ -37,11 +38,25 @@ class Game(ge.Platformer):
         self.create_tower(2, 3, coords=(24, 1))
         self.create_tower(5, 3, coords=(26, 1))
 
-        self.create_platform(3, coords=(34, 3))
+        self.create_platform(3, coords=(33, 3))
 
         self.create_tower(2, 3, coords=(42, 1))
+        self.create_fence('left', (42, 3))
+        self.create_fence('right', (43, 3))
+        self.create_tower(2, 3, coords=(46, 1))
+        self.create_fence('left', (47, 3))
+        self.create_fence('left', (48, 3))
         self.create_tower(5, 3, coords=(44, 1))
+
+        self.create_platform(3, coords=(52, 5))
+
+        self.create_platform(3, coords=(57, 3))        
+
+        self.create_tower(2, 3, coords=(65, 1))
+        self.create_tower(5, 3, coords=(63, 1))
         
+            
+        # Spikes 
         self.spike = self.create_object('other/spikes/spikes-high', (20, 1))
         self.spike = self.create_object('other/spikes/spikes-high', (17, 1))
         self.spike = self.create_object('other/spikes/spikes-low', (18, 1))
@@ -49,6 +64,7 @@ class Game(ge.Platformer):
         self.spike = self.create_object('other/spikes/spikes-low', (21, 1))
         self.spike = self.create_object('other/spikes/spikes-low', (22, 1))
         self.spike = self.create_object('other/spikes/spikes-high', (23, 1))
+        
         
         self.spike = self.create_object('other/spikes/spikes-high', (40, 1))
         self.spike = self.create_object('other/spikes/spikes-low', (41, 1))
@@ -64,6 +80,10 @@ class Game(ge.Platformer):
         self.spike = self.create_object('other/spikes/spikes-low', (30, 1))
         self.spike = self.create_object('other/spikes/spikes-high', (29, 1))
 
+        self.spike = self.create_object('other/spikes/spikes-high', (53, 6))
+
+        self.spike = self.create_object('other/spikes/spikes-high', (58, 4))
+
         self.background_near = SpriteList(use_spatial_hash=False)
         self.background_fixed = SpriteList(use_spatial_hash=False)
         self.background_fixed.append(Sprite(get_sprite_path('background/bg1')))
@@ -73,6 +93,8 @@ class Game(ge.Platformer):
         self.enemy = self.create_object('enemy/enemyFloating_1', (10, 5), at=self.enemies)
 
     def init_items(self):
+
+        #Coins
         self.items = SpriteList()
         self.item = self.create_object('other/items/greenCrystal', (1, 11), at=self.items)
 
@@ -101,6 +123,23 @@ class Game(ge.Platformer):
 
         self.item = self.create_object('other/items/yellowGem', (24, 3), at=self.items)
         self.item = self.create_object('other/items/yellowGem', (25, 3), at=self.items)
+
+        self.item = self.create_object('other/items/yellowGem', (44, 6), at=self.items)
+        self.item = self.create_object('other/items/yellowGem', (45, 6), at=self.items)
+        self.item = self.create_object('other/items/yellowGem', (46, 6), at=self.items)
+
+        self.item = self.create_object('other/items/yellowGem', (52, 6), at=self.items)
+        self.item = self.create_object('other/items/yellowGem', (54, 6), at=self.items)
+
+        self.item = self.create_object('other/items/yellowGem', (57, 4), at=self.items)
+        self.item = self.create_object('other/items/yellowGem', (59, 4), at=self.items)
+
+        self.item = self.create_object('other/items/yellowGem', (63, 6), at=self.items)
+        self.item = self.create_object('other/items/yellowGem', (64, 6), at=self.items)
+        self.item = self.create_object('other/items/yellowGem', (65, 6), at=self.items)
+        
+        self.item = self.create_object('other/items/yellowGem', (66, 3), at=self.items)
+        self.item = self.create_object('other/items/yellowGem', (67, 3), at=self.items)
 
     def init(self):
         self.init_world()
